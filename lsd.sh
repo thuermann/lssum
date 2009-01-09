45020fbfe0fec577832e0fff30dd539813a8919e
Urs Thuermann <urs@isnogud.escape.de>
2009-01-09 14:18:40 +0100

    Only check directories, regular files, and symlinks

--- lsd.sh
+++ lsd.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #
-# $Id: lsd.sh,v 1.1 2008/12/05 16:07:42 urs Exp $
+# $Id: lsd.sh,v 1.2 2009/01/09 13:18:40 urs Exp $
+
+types="-type d -o -type f -o -type l"
 
 for arg in "$@"; do (
     if [ -d "$arg" ]; then
 	cd "$arg"
-	find -print0 | sort -z | xargs -0 lssum | md5sum |
+	find \( $types \) -print0 | sort -z | xargs -0 lssum | md5sum |
 		sed "s|-|$arg|"
     else
 	echo .
--- lsdiff.sh
+++ lsdiff.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
-# $Id: lsdiff.sh,v 1.1 2008/12/05 16:07:42 urs Exp $
+# $Id: lsdiff.sh,v 1.2 2009/01/09 13:18:40 urs Exp $
 
 test -d "$1" -a -d "$2" || { echo error ; exit 1 ; }
 
-(cd "$1"; find -print0 | sort -z | xargs -0 lssum >/tmp/lss.1)
-(cd "$2"; find -print0 | sort -z | xargs -0 lssum >/tmp/lss.2)
+types="-type d -o -type f -o -type l"
+
+(cd "$1"; find \( $types \) -print0 | sort -z | xargs -0 lssum >/tmp/lss.1)
+(cd "$2"; find \( $types \) -print0 | sort -z | xargs -0 lssum >/tmp/lss.2)
 
 diff -u /tmp/lss.1 /tmp/lss.2
