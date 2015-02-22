37bf78436184630244032db185b87484971e30be
Urs Thuermann <urs@isnogud.escape.de>
2015-02-22 23:40:37 +0100

    Return exit code 1 on failures

--- lssum.c
+++ lssum.c
@@ -1,5 +1,5 @@
 /*
- * $Id: lssum.c,v 1.16 2012/04/04 17:56:26 urs Exp $
+ * $Id: lssum.c,v 1.17 2015/02/22 22:40:37 urs Exp $
  */
 
 #include <stdio.h>
@@ -16,7 +16,7 @@
 
 #define BUFSIZE 65536
 
-static void lssum(const char *fname);
+static int lssum(const char *fname);
 static unsigned char *md5(const char *fname);
 
 static int opt_mtime   = 0;
@@ -64,12 +64,13 @@ int main(int argc, char **argv)
 	usage(argv[0]);
 
     for (i = optind; i < argc; i++)
-	lssum(argv[i]);
+	if (lssum(argv[i]))
+	    errflg = 1;
 
-    return 0;
+    return errflg;
 }
 
-static void lssum(const char *fname)
+static int lssum(const char *fname)
 {
     struct stat st;
     char ts[sizeof("YYYY-MM-DD HH:MM:SS +0000 YYYY-MM-DD HH:MM:SS +0000")];
@@ -79,7 +80,7 @@ static void lssum(const char *fname)
 
     if (lstat(fname, &st) < 0) {
 	perror(fname);
-	return;
+	return 1;
     }
     tm = localtime(&st.st_mtime);
     strftime(ts, sizeof(ts), "%F %T %z", tm);
@@ -89,25 +90,27 @@ static void lssum(const char *fname)
     }
     if (S_ISDIR(st.st_mode)) {
 	printf("%-44s  %s  %s\n", "dir", ts, fname);
-	return;
+	return 0;
     } else if (S_ISLNK(st.st_mode)) {
 	char sym[PATH_MAX];
 	int  len = readlink(fname, sym, PATH_MAX - 1);
 	if (len < 0) {
 	    perror(fname);
-	    return;
+	    return 1;
 	}
 	sym[len] = 0;
 	printf("%-44s  %s  %s -> %s\n", "sym", ts, fname, sym);
-	return;
+	return 0;
     }
 
     if (!(hash = md5(fname)))
-	return;
+	return 1;
 
     for (i = 0; i < MD5_DIGEST_LENGTH; i++)
 	printf("%02x", hash[i]);
     printf("  %10lld  %s  %s\n", (long long)st.st_size, ts, fname);
+
+    return 0;
 }
 
 static unsigned char *md5(const char *fname)
@@ -125,8 +128,10 @@ static unsigned char *md5(const char *fname)
     MD5_Init(&ctx);
     while ((nbytes = read(fd, buffer, sizeof(buffer))) > 0)
 	MD5_Update(&ctx, buffer, nbytes);
-    if (nbytes < 0)
+    if (nbytes < 0) {
 	perror(fname);
+	return NULL;
+    }
     MD5_Final(hash, &ctx);
 
     close(fd);
